fcb273d91f995201052cab7e76aa480ebc53b698
LJ Web Development <ljwebdevelopmentok@gmail.com>
2026-02-18 16:18:07 -0600

    Delete js directory

--- js/firebase-init.js
+++
@@ -1,9 +0,0 @@
-// Firebase initialization will be wired in Prompt 2.
-// Keep one modular initialization entry point for the whole app.
-export function initializeFirebase() {
-  return {
-    app: null,
-    auth: null,
-    db: null
-  };
-}
